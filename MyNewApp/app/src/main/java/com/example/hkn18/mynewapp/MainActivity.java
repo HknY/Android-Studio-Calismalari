561e2979d36921f93872248ecd419b828c511ad7
Hakan Yılmaz <hkn1899@gmail.com>
2018-04-24 16:48:22 +0300

    imageview
    switch ile random sayıyla resim değiştirme

--- MyNewApp/app/src/main/java/com/example/hkn18/mynewapp/MainActivity.java
+++ MyNewApp/app/src/main/java/com/example/hkn18/mynewapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.hkn18.mynewapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    int sayi = 1;
+
+    public void degis(View view){
+
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+
+        Random a = new Random();
+        sayi = a.nextInt(2);
+
+        switch (sayi){
+            case 0:
+                imageView.setImageResource(R.drawable.tesla2);
+                break;
+
+            case 1:
+                imageView.setImageResource(R.drawable.tesla11);
+                break;
+        }
+
+    }
+
 }
