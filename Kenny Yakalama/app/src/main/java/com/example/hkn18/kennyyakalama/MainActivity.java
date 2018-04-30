662f59f8bb12aea15e1e974d068ec22fdf9e5d3b
Hakan Yılmaz <hkn1899@gmail.com>
2018-04-30 11:47:12 +0300

    kenny yakalama
    kenny yakalama arayüzünde titreşim için switch eklendi

--- Kenny Yakalama/app/src/main/java/com/example/hkn18/kennyyakalama/MainActivity.java
+++ Kenny Yakalama/app/src/main/java/com/example/hkn18/kennyyakalama/MainActivity.java
@@ -1,17 +1,18 @@
 package com.example.hkn18.kennyyakalama;
 
 import android.annotation.SuppressLint;
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -20,6 +21,10 @@ public class MainActivity extends AppCompatActivity {
 
     TextView sure;
     TextView skorum;
+    TextView bestSkor;
+    Button basla;
+    Switch titre;
+    Vibrator vib;
 
     ImageView k1;
     ImageView k2;
@@ -36,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
     Runnable run;
     int skoru = 0;
     int index;
+    boolean chec = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +51,11 @@ public class MainActivity extends AppCompatActivity {
         best = this.getSharedPreferences("com.example.hkn18.kennyyakalama", Context.MODE_PRIVATE);
 
         skorum = findViewById(R.id.skor);
+        sure = findViewById(R.id.sure);
+        basla = findViewById(R.id.btnoyna);
+        bestSkor = findViewById(R.id.bestSkor);
+        titre = findViewById(R.id.switch1);
+
         k1 = findViewById(R.id.imageView);
         k2 = findViewById(R.id.imageView2);
         k3 = findViewById(R.id.imageView3);
@@ -56,10 +67,29 @@ public class MainActivity extends AppCompatActivity {
         k9 = findViewById(R.id.imageView9);
 
         resimler = new ImageView[] {k1, k2, k3, k4, k5, k6, k7, k8, k9};
+
+        if(best.getInt("skor",0) == 0){
+            bestSkor.setText("En Yüksek Skor\n\t0");
+        }
+        else{
+            bestSkor.setText("En Yüksek Skor\n\t\t\t\t\t" + best.getInt("skor",0) );
+        }
+
+        titre.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                chec = isChecked;
+            }
+        });
+
         sakla();
+    }
 
-        sure = findViewById(R.id.sure);
-
+    public void oyna(View view){
+        skorum.setText("Skor : 0");
+        skoru = 0;
+        hareket();
+        basla.setVisibility(View.INVISIBLE);
         new CountDownTimer(10000,1000){
 
             @SuppressLint("SetTextI18n")
@@ -71,54 +101,40 @@ public class MainActivity extends AppCompatActivity {
             @SuppressLint("SetTextI18n")
             @Override
             public void onFinish() {
-                sure.setText("Süre Doldu");
+
                 handler.removeCallbacks(run);
+                sakla();
+                basla.setVisibility(View.VISIBLE);
                 resimler[index].setVisibility(View.INVISIBLE);
 
                 if(best.getInt("skor",0) == 0){
                     best.edit().putInt("skor",skoru).apply();
-                    alert("Skorunuz : " + skoru);
+                    bestSkor.setText("En Yüksek Skor\n\t0");
                 }
                 else{
                     if(best.getInt("skor",0) < skoru){
                         best.edit().putInt("skor",skoru).apply();
-                        alert("Tebrikler\nEn Yüksek Skor : " + best.getInt("skor",0) + "\nSizin Skorunuz  : " + skoru);
                     }
-                    else
-                        alert("En Yüksek Skor : " + best.getInt("skor",0) + "\nSizin Skorunuz  : " + skoru);
+                    bestSkor.setText("En Yüksek Skor\n\t\t\t\t\t" + best.getInt("skor",0) );
                 }
+                sure.setText("Süre Doldu");
+
             }
         }.start();
     }
 
+
     @SuppressLint("SetTextI18n")
     public void arttir (View view){
         skorum.setText("Skor : " + ++skoru);
+        vib = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        if(chec){
+            vib.vibrate(200);
+        }
+
     }
 
-    public void alert(String yazı){
-        AlertDialog.Builder tablo = new AlertDialog.Builder(this);
-        tablo.setTitle("SKOR");
-        tablo.setMessage(yazı + "\n\nTekrar Oynamak İster Misiniz?");
-        tablo.setPositiveButton("Evet", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                Intent i = getBaseContext().getPackageManager().getLaunchIntentForPackage(getBaseContext().getPackageName());
-                assert i != null;
-                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(i);
-            }
-        });
-        tablo.setNegativeButton("Hayır", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                System.exit(0);
-            }
-        });
-        tablo.show();
-    }
-
-    public void sakla(){
+    public void hareket(){
 
         handler = new Handler();
         run = new Runnable() {
@@ -132,10 +148,17 @@ public class MainActivity extends AppCompatActivity {
                 index = r.nextInt(8);
                 resimler[index].setVisibility(View.VISIBLE);
 
-                handler.postDelayed(this, 300);
+                handler.postDelayed(this, 400);
             }
         };
-
         handler.post(run);
+    }
+
+    public void sakla(){
+        for(ImageView resim : resimler){
+            resim.setVisibility(View.INVISIBLE);
+        }
+        resimler[7].setVisibility(View.VISIBLE);
+        resimler[7].setEnabled(false);
     }
 }
